5b4d8a775ec2e91516f1d8513adf24f3a002fdb4
Ivan Petrushin <ivan.kiel@gmail.com>
2018-12-19 18:57:59 +0800

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int[] a = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            // считать число
+            a[i] = sc.nextInt();
+            // записать число в ячейку массива
+        }
+        int n = 0;
+        for (int i = 0; i < k-1; i++) {
+            if (a[i+1] > a[i])
+            {
+                n++;
+            }
+        }
+        System.out.println(n);
+
+    }
+}
